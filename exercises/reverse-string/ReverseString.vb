a70370b413c44985233d4fb5420d03a2cbb76a04
ch020 <chappersthepenguin@gmail.com>
2021-01-14 13:37:53 +0000

    reverse-string: Added the Reverse String exercise
    Added the Reverse String exercise to the VB.NET track

---
+++ exercises/reverse-string/ReverseString.vb
@@ -0,0 +1,5 @@
+﻿Public Module ReverseString
+    Public Function Reverse(ByVal inputString As String) As String
+        Throw New NotImplementedException("Delete this statement and write your own implementation here")
+    End Function
+End Module
