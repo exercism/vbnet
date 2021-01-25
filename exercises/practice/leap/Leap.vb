e12a83455d2f439fc0e00bf5bdaebcfb691a5fc0
ch020 <chappersthepenguin@gmail.com>
2021-01-25 12:39:10 +0000

    leap: Added the leap exercise
    Added the leap exercise to the VB.NET track

---
+++ exercises/practice/leap/Leap.vb
@@ -0,0 +1,5 @@
+﻿Public Module Leap
+    Public Function IsLeapYear(ByVal year As Integer) As Boolean
+        Throw New NotImplementedException("Delete this statement and write your own implementation here")
+    End Function
+End Module
