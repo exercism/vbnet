c401eb619cf6484cfe7243e7747d0f7f90e9ebb6
Bruce M. Axtens <bugmagnet@outlook.com>
2021-06-24 22:34:14 +0800

    improving tests

--- exercises/practice/binary/BinaryTests.vb
+++ exercises/practice/binary/BinaryTests.vb
@@ -3,13 +3,14 @@ Imports XUnit
 
 Public Class BinaryTest
     ' change Ignore to false to run test case or just remove 'Ignore = true'
-    <TestCase("1", Result:=1)>
-    <TestCase("10", Result:=2, Ignore:=True)>
-    <TestCase("11", Result:=3, Ignore:=True)>
-    <TestCase("100", Result:=4, Ignore:=True)>
-    <TestCase("1001", Result:=9, Ignore:=True)>
-    <TestCase("11010", Result:=26, Ignore:=True)>
-    <TestCase("10001101000", Result:=1128, Ignore:=True)>
+    <Theory>
+    <InLineData("1", Result:=1)>
+    <InLineData("10", Result:=2, Ignore:=True)>
+    <InLineData("11", Result:=3, Ignore:=True)>
+    <InLineData("100", Result:=4, Ignore:=True)>
+    <InLineData("1001", Result:=9, Ignore:=True)>
+    <InLineData("11010", Result:=26, Ignore:=True)>
+    <InLineData("10001101000", Result:=1128, Ignore:=True)>
     Public Function BinaryConvertsToDecimal(binary As String) As Integer
         Return New Binary(binary).ToDecimal()
     End Function
@@ -21,11 +22,11 @@ Public Class BinaryTest
     <TestCase("134678", Ignore:=True)>
     <TestCase("abc10z", Ignore:=True)>
     Public Sub InvalidBinaryIsDecimal0(invalidValue As String)
-        Assert.That(New Binary(invalidValue).ToDecimal(), [Is].EqualTo(0))
+        Assert.Equal(New Binary(invalidValue).ToDecimal(), 0)
     End Sub
 
 <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub BinaryCanConvertFormattedStrings()
-        Assert.That(New Binary("011").ToDecimal(), [Is].EqualTo(3))
+        Assert.Equal(New Binary("011").ToDecimal(), 3)
     End Sub
 End Class
--- exercises/practice/bob/BobTests.vb
+++ exercises/practice/bob/BobTests.vb
@@ -11,86 +11,86 @@ Public Class BobTest
 
     <Fact>
     Public Sub StatingSomething()
-        Assert.That(teenager.Hey("Tom-ay-to, tom-aaaah-to."), [Is].EqualTo("Whatever."))
+        Assert.Equal(teenager.Hey("Tom-ay-to, tom-aaaah-to."), "Whatever.")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub Shouting()
-        Assert.That(teenager.Hey("WATCH OUT!"), [Is].EqualTo("Whoa, chill out!"))
+        Assert.Equal(teenager.Hey("WATCH OUT!"), "Whoa, chill out!")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub AskingAQuestion()
-        Assert.That(teenager.Hey("Does this cryogenic chamber make me look fat?"), [Is].EqualTo("Sure."))
+        Assert.Equal(teenager.Hey("Does this cryogenic chamber make me look fat?"), "Sure.")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub AskingANumericQuestion()
-        Assert.That(teenager.Hey("You are, what, like 15?"), [Is].EqualTo("Sure."))
+        Assert.Equal(teenager.Hey("You are, what, like 15?"), "Sure.")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub TalkingForcefully()
-        Assert.That(teenager.Hey("Let's go make out behind the gym!"), [Is].EqualTo("Whatever."))
+        Assert.Equal(teenager.Hey("Let's go make out behind the gym!"), "Whatever.")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub UsingAcronymsInRegularSearch()
-        Assert.That(teenager.Hey("It's OK if you don't want to go to the DMV."), [Is].EqualTo("Whatever."))
+        Assert.Equal(teenager.Hey("It's OK if you don't want to go to the DMV."), "Whatever.")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub ForcefulQuestions()
-        Assert.That(teenager.Hey("WHAT THE HELL WERE YOU THINKING?"), [Is].EqualTo("Whoa, chill out!"))
+        Assert.Equal(teenager.Hey("WHAT THE HELL WERE YOU THINKING?"), "Whoa, chill out!")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub ShoutingNumbers()
-        Assert.That(teenager.Hey("1, 2, 3 GO!"), [Is].EqualTo("Whoa, chill out!"))
+        Assert.Equal(teenager.Hey("1, 2, 3 GO!"), "Whoa, chill out!")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub OnlyNumbers()
-        Assert.That(teenager.Hey("1, 2, 3"), [Is].EqualTo("Whatever."))
+        Assert.Equal(teenager.Hey("1, 2, 3"), "Whatever.")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub QuestionWithOnlyNumbers()
-        Assert.That(teenager.Hey("4?"), [Is].EqualTo("Sure."))
+        Assert.Equal(teenager.Hey("4?"), "Sure.")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub ShoutingWithSpecialCharacters()
-        Assert.That(teenager.Hey("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!"), [Is].EqualTo("Whoa, chill out!"))
+        Assert.Equal(teenager.Hey("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!"), "Whoa, chill out!")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub ShoutingWithNoExclamationMark()
-        Assert.That(teenager.Hey("I HATE YOU"), [Is].EqualTo("Whoa, chill out!"))
+        Assert.Equal(teenager.Hey("I HATE YOU"), "Whoa, chill out!")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub StatementContainingQuestionMark()
-        Assert.That(teenager.Hey("Ending with ? means a question."), [Is].EqualTo("Whatever."))
+        Assert.Equal(teenager.Hey("Ending with ? means a question."), "Whatever.")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub PrattlingOn()
-        Assert.That(teenager.Hey("Wait! Hang on. Are you going to be OK?"), [Is].EqualTo("Sure."))
+        Assert.Equal(teenager.Hey("Wait! Hang on. Are you going to be OK?"), "Sure.")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub Silence()
-        Assert.That(teenager.Hey(""), [Is].EqualTo("Fine. Be that way!"))
+        Assert.Equal(teenager.Hey(""), "Fine. Be that way!")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub ProlongedSilence()
-        Assert.That(teenager.Hey("    "), [Is].EqualTo("Fine. Be that way!"))
+        Assert.Equal(teenager.Hey("    "), "Fine. Be that way!")
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
+    <Fact(Skip := "Remove this Skip property to run this test")>
     Public Sub MultipleLineQuestion()
-        Assert.That(teenager.Hey("Does this cryogenic chamber make me look fat?" & vbLf & "no"), [Is].EqualTo("Whatever."))
+        Assert.Equal(teenager.Hey("Does this cryogenic chamber make me look fat?" & vbLf & "no"), "Whatever.")
     End Sub
 End Class
