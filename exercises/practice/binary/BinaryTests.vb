c9caba784eab8ce1fc943dbdb5895f8676ff116e
Bruce M. Axtens <bugmagnet@outlook.com>
2021-06-25 08:22:19 +0800

    converted NUnit to XUnit

--- exercises/practice/binary/BinaryTests.vb
+++ exercises/practice/binary/BinaryTests.vb
@@ -2,31 +2,32 @@ Imports XUnit
 
 
 Public Class BinaryTest
-    ' change Ignore to false to run test case or just remove 'Ignore = true'
-    <Theory>
-    <InLineData("1", Result:=1)>
-    <InLineData("10", Result:=2, Ignore:=True)>
-    <InLineData("11", Result:=3, Ignore:=True)>
-    <InLineData("100", Result:=4, Ignore:=True)>
-    <InLineData("1001", Result:=9, Ignore:=True)>
-    <InLineData("11010", Result:=26, Ignore:=True)>
-    <InLineData("10001101000", Result:=1128, Ignore:=True)>
-    Public Function BinaryConvertsToDecimal(binary As String) As Integer
-        Return New Binary(binary).ToDecimal()
-    End Function
+	<Fact>
+    Public Sub BinaryCanConvertFormattedStrings()
+        Assert.Equal(New Binary("011").ToDecimal(), 3)
+    End Sub
 
-    <TestCase("carrot", Ignore:=True)>
-    <TestCase("2", Ignore:=True)>
-    <TestCase("5", Ignore:=True)>
-    <TestCase("9", Ignore:=True)>
-    <TestCase("134678", Ignore:=True)>
-    <TestCase("abc10z", Ignore:=True)>
+    <Theory(Skip := "Remove this Skip property to run this test")>
+    <InLineData("1", 1)>
+    <InLineData("10", 2)>
+    <InLineData("11", 3)>
+    <InLineData("100", 4)>
+    <InLineData("1001", 9)>
+    <InLineData("11010", 26)>
+    <InLineData("10001101000", 1128)>
+    Public Sub BinaryConvertsToDecimal(binary As String, expected as integer)
+        Assert.Equal( New Binary(binary).ToDecimal(), expected)
+    End Sub
+
+	<Theory(Skip := "Remove this Skip property to run this test")>
+    <InLineData("carrot")>
+    <InLineData("2")>
+    <InLineData("5")>
+    <InLineData("9")>
+    <InLineData("134678")>
+    <InLineData("abc10z")>
     Public Sub InvalidBinaryIsDecimal0(invalidValue As String)
         Assert.Equal(New Binary(invalidValue).ToDecimal(), 0)
     End Sub
 
-<Fact(Skip := "Remove this Skip property to run this test")>
-    Public Sub BinaryCanConvertFormattedStrings()
-        Assert.Equal(New Binary("011").ToDecimal(), 3)
-    End Sub
 End Class
