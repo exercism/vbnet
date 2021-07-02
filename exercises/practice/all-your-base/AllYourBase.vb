c54c56f0fa233d12db65d60af33f089314a503c2
Bruce M. Axtens <bugmagnet@outlook.com>
2021-07-02 17:27:04 +0800

    Almost empty framework

---
+++ exercises/practice/all-your-base/AllYourBase.vb
@@ -0,0 +1,5 @@
+Public Module AllYourBase
+    Public Function Rebase(ByVal inputBase As Integer, ByVal inputDigits As Integer(), ByVal outputBase As Integer) As Integer()
+        Throw New NotImplementedException("Delete this statement and write your own implementation here")
+    End Function
+End Module
