871622b524832035698e54205eeeb2c998e2c6ac
Bruce M. Axtens <bugmagnet@outlook.com>
2021-07-02 17:23:42 +0800

    all-your-base plus leap's toml

---
+++ exercises/practice/all-your-base/.meta/Example.vb
@@ -0,0 +1,26 @@
+Imports System
+Imports System.Collections.Generic
+
+Public Module AllYourBase
+    Public Function Rebase(ByVal inputBase As Integer, ByVal inputDigits As Integer(), ByVal outputBase As Integer) As Integer()
+        If outputBase <= 1 OrElse inputBase <= 1 Then Throw New ArgumentException()
+        Dim inputValue = 0
+        Dim exponent = 0, place = inputDigits.Length - 1
+
+        While exponent < inputDigits.Length
+            If inputDigits(place) < 0 OrElse inputDigits(place) >= inputBase Then Throw New ArgumentException()
+            inputValue += inputDigits(place) * CInt(Math.Pow(inputBase, exponent))
+            exponent += 1
+            place -= 1
+        End While
+
+        Dim outputList As List(Of Integer) = New List(Of Integer)()
+
+        Do
+            outputList.Insert(0, inputValue Mod outputBase)
+            inputValue /= outputBase
+        Loop While inputValue > 0
+
+        Return outputList.ToArray()
+    End Function
+End Module
---
+++ exercises/practice/all-your-base/AllYourBaseTests.vb
@@ -0,0 +1,164 @@
+' This file was auto-generated based on version 2.3.0 of the canonical data.
+Imports System
+Imports Xunit
+Public Class AllYourBaseTests
+    <Fact>
+    Public Sub SingleBitOneToDecimal()
+        Dim inputBase = 2
+        Dim digits = {1}
+        Dim outputBase = 10
+        Dim expected = {1}
+        Assert.Equal(expected, Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub BinaryToSingleDecimal()
+        Dim inputBase = 2
+        Dim digits = {1, 0, 1}
+        Dim outputBase = 10
+        Dim expected = {5}
+        Assert.Equal(expected, Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub SingleDecimalToBinary()
+        Dim inputBase = 10
+        Dim digits = {5}
+        Dim outputBase = 2
+        Dim expected = {1, 0, 1}
+        Assert.Equal(expected, Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub BinaryToMultipleDecimal()
+        Dim inputBase = 2
+        Dim digits = {1, 0, 1, 0, 1, 0}
+        Dim outputBase = 10
+        Dim expected = {4, 2}
+        Assert.Equal(expected, Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub DecimalToBinary()
+        Dim inputBase = 10
+        Dim digits = {4, 2}
+        Dim outputBase = 2
+        Dim expected = {1, 0, 1, 0, 1, 0}
+        Assert.Equal(expected, Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub TrinaryToHexadecimal()
+        Dim inputBase = 3
+        Dim digits = {1, 1, 2, 0}
+        Dim outputBase = 16
+        Dim expected = {2, 10}
+        Assert.Equal(expected, Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub HexadecimalToTrinary()
+        Dim inputBase = 16
+        Dim digits = {2, 10}
+        Dim outputBase = 3
+        Dim expected = {1, 1, 2, 0}
+        Assert.Equal(expected, Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub Number15BitInteger()
+        Dim inputBase = 97
+        Dim digits = {3, 46, 60}
+        Dim outputBase = 73
+        Dim expected = {6, 10, 45}
+        Assert.Equal(expected, Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub EmptyList()
+        Dim inputBase = 2
+        Dim digits = Array.Empty(Of Integer)()
+        Dim outputBase = 10
+        Dim expected = {0}
+        Assert.Equal(expected, Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub SingleZero()
+        Dim inputBase = 10
+        Dim digits = {0}
+        Dim outputBase = 2
+        Dim expected = {0}
+        Assert.Equal(expected, Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub MultipleZeros()
+        Dim inputBase = 10
+        Dim digits = {0, 0, 0}
+        Dim outputBase = 2
+        Dim expected = {0}
+        Assert.Equal(expected, Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub LeadingZeros()
+        Dim inputBase = 7
+        Dim digits = {0, 6, 0}
+        Dim outputBase = 10
+        Dim expected = {4, 2}
+        Assert.Equal(expected, Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub InputBaseIsOne()
+        Dim inputBase = 1
+        Dim digits = {0}
+        Dim outputBase = 10
+        Assert.Throws(Of ArgumentException)(Function() Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub InputBaseIsZero()
+        Dim inputBase = 0
+        Dim digits = Array.Empty(Of Integer)()
+        Dim outputBase = 10
+        Assert.Throws(Of ArgumentException)(Function() Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub InputBaseIsNegative()
+        Dim inputBase = -2
+        Dim digits = {1}
+        Dim outputBase = 10
+        Assert.Throws(Of ArgumentException)(Function() Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub NegativeDigit()
+        Dim inputBase = 2
+        Dim digits = {1, -1, 1, 0, 1, 0}
+        Dim outputBase = 10
+        Assert.Throws(Of ArgumentException)(Function() Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub InvalidPositiveDigit()
+        Dim inputBase = 2
+        Dim digits = {1, 2, 1, 0, 1, 0}
+        Dim outputBase = 10
+        Assert.Throws(Of ArgumentException)(Function() Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub OutputBaseIsOne()
+        Dim inputBase = 2
+        Dim digits = {1, 0, 1, 0, 1, 0}
+        Dim outputBase = 1
+        Assert.Throws(Of ArgumentException)(Function() Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub OutputBaseIsZero()
+        Dim inputBase = 10
+        Dim digits = {7}
+        Dim outputBase = 0
+        Assert.Throws(Of ArgumentException)(Function() Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub OutputBaseIsNegative()
+        Dim inputBase = 2
+        Dim digits = {1}
+        Dim outputBase = -7
+        Assert.Throws(Of ArgumentException)(Function() Rebase(inputBase, digits, outputBase))
+    End Sub
+    <Fact(Skip := "Remove this Skip property to run this test")>
+    Public Sub BothBasesAreNegative()
+        Dim inputBase = -2
+        Dim digits = {1}
+        Dim outputBase = -7
+        Assert.Throws(Of ArgumentException)(Function() Rebase(inputBase, digits, outputBase))
+    End Sub
+End Class
