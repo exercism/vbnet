54dabe068e9c4d90b5edff46918760b59a55c04c
Bruce Axtens <bruce.axtens@gmail.com>
2021-07-07 18:30:17 +0800

    removal of spurious file

--- exercises/practice/phone-number/HelloWorld.vb
+++
@@ -1,5 +0,0 @@
-Public Class PhoneNumber
-    Public Shared Function Clean(ByVal phoneNumber As String) As String
-        Throw New NotImplementedException("You need to implement this function")
-    End Function
-End Module
