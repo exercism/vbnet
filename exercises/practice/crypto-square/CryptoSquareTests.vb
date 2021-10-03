21a7e534ba290ed1bacc1a7fcdbff13346c2bd7b
Erik Schierboom <erik_schierboom@hotmail.com>
2021-10-03 19:01:47 +0200

    crypto-square: update to latest canonical data

--- exercises/practice/crypto-square/CryptoSquareTests.vb
+++ exercises/practice/crypto-square/CryptoSquareTests.vb
@@ -2,80 +2,44 @@ Imports XUnit
 
 Public Class CryptoSquareTest
     <Fact>
-    Public Sub StrangeCharactersAreStrippedDuringNormalization()
-        Dim crypto = New Crypto("s#$%^&plunk")
-        Assert.Equal(crypto.NormalizePlaintext, "splunk")
+    Public Sub EmptyPlaintextResultsInAnEmptyplaintext()
+        Dim plaintext = ""
+        Assert.Equal("", Ciphertext(plaintext))
+    End Sub
+    
+    <Fact(Skip:="Remove this Skip property to run this test")>
+    Public Sub Lowercase()
+        Dim plaintext = "A"
+        Assert.Equal("a", Ciphertext(plaintext))
+    End Sub
+    
+    <Fact(Skip:="Remove this Skip property to run this test")>
+    Public Sub RemoveSpaces()
+        Dim plaintext = "  b "
+        Assert.Equal("b", Ciphertext(plaintext))
+    End Sub
+    
+    <Fact(Skip:="Remove this Skip property to run this test")>
+    Public Sub RemovePunctuation()
+        Dim plaintext = "@1,%!"
+        Assert.Equal("1", Ciphertext(plaintext))
+    End Sub
+    
+    <Fact(Skip:="Remove this Skip property to run this test")>
+    Public Sub NineCharacterPlaintextResultsInThreeChunksOfThreeCharacters()
+        Dim plaintext = "This is fun!"
+        Assert.Equal("tsf hiu isn", Ciphertext(plaintext))
     End Sub
 
-	<Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub LettersAreLowercasedDuringNormalization()
-        Dim crypto = New Crypto("WHOA HEY!")
-        Assert.Equal(crypto.NormalizePlaintext, "whoahey")
+    <Fact(Skip:="Remove this Skip property to run this test")>
+    Public Sub EightCharacterPlaintextResultsInThreeChunksTheLastOneWithATrailingSpace()
+        Dim plaintext = "Chill out."
+        Assert.Equal("clu hlt io ", Ciphertext(plaintext))
     End Sub
 
-	<Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub NumbersAreKeptDuringNormalization()
-        Dim crypto = New Crypto("1, 2, 3, GO!")
-        Assert.Equal(crypto.NormalizePlaintext, "123go")
-    End Sub
-
-	<Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub SmallestSquareSizeIs2()
-        Dim crypto = New Crypto("1234")
-        Assert.Equal(crypto.Size, 2)
-    End Sub
-
-	<Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub SizeOfTextWhoseLengthIsAPerfectSquareIsItsSquareRoot()
-        Dim crypto = New Crypto("123456789")
-        Assert.Equal(crypto.Size, 3)
-    End Sub
-
-	<Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub SizeOfTextWhoseLengthIsNotAPerfectSquareIsNextBiggestSquareRoot()
-        Dim crypto = New Crypto("123456789abc")
-        Assert.Equal(crypto.Size, 4)
-    End Sub
-
-	<Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub SizeIsDeterminedByNormalizedText()
-        Dim crypto = New Crypto("Oh hey, this is nuts!")
-        Assert.Equal(crypto.Size, 4)
-    End Sub
-
-	<Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub SegmentsAreSplitBySquareSize()
-        Dim crypto = New Crypto("Never vex thine heart with idle woes")
-        Assert.Equal(crypto.PlaintextSegments(), New String() {"neverv", "exthin", "eheart", "withid", "lewoes"})
-    End Sub
-
-	<Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub SegmentsAreSplitBySquareSizeUntilTextRunsOut()
-        Dim crypto = New Crypto("ZOMG! ZOMBIES!!!")
-        Assert.Equal(crypto.PlaintextSegments(), New String() {"zomg", "zomb", "ies"})
-    End Sub
-
-	<Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub CiphertextCombinesTextByColumn()
-        Dim crypto = New Crypto("First, solve the problem. Then, write the code.")
-        Assert.Equal(crypto.Ciphertext(), "foeewhilpmrervrticseohtottbeedshlnte")
-    End Sub
-
-	<Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub CiphertextSkipsCellsWithNoText()
-        Dim crypto = New Crypto("Time is an illusion. Lunchtime doubly so.")
-        Assert.Equal(crypto.Ciphertext(), "tasneyinicdsmiohooelntuillibsuuml")
-    End Sub
-
-	<Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub NormalizedCiphertextIsSplitBy5()
-        Dim crypto = New Crypto("Vampires are people too!")
-        Assert.Equal(crypto.NormalizeCiphertext(), "vrela epems etpao oirpo")
-    End Sub
-
-	<Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub NormalizedCiphertextNotExactlyDivisibleBy5SpillsIntoASmallerSegment()
-        Dim crypto = New Crypto("Madness, and then illumination.")
-        Assert.Equal(crypto.NormalizeCiphertext(), "msemo aanin dninn dlaet ltshu i")
+    <Fact(Skip:="Remove this Skip property to run this test")>
+    Public Sub FiftyFourCharacterPlaintextResultsInSevenChunksTheTwoWithTrailingSpaces()
+        Dim plaintext = "If man was meant to stay on the ground, god would have given us roots."
+        Assert.Equal("imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau ", Ciphertext(plaintext))
     End Sub
 End Class
