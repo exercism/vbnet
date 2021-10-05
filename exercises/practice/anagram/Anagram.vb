ce977e1a4d03daaf198ffe94f1656f1be4dc1c62
Christoph Hafner <Christoph.Hafner@hitachi-powergrids.com>
2021-10-05 23:23:25 +0200

    Remove compile errors.

--- exercises/practice/anagram/Anagram.vb
+++ exercises/practice/anagram/Anagram.vb
@@ -1,3 +1,11 @@
 Public Class Anagram
 
+    Public Sub New(word As String)
+        Throw New NotImplementedException("It's up to you to implement this code...")
+    End Sub
+
+    Public Function Match(words As IEnumerable(Of String)) As IEnumerable(Of String)
+        Throw New NotImplementedException("It's up to you to implement this code...")
+    End Function
+
 End Class
