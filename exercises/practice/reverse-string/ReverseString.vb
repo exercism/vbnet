c8050028a385839de727fa56996fb8f25b804d1d
Herbert Mertens <hmertens@hmccs.com>
2023-04-15 13:43:21 -0700

    Implementation for Test:An_empty_string()
    Test for an empty string; return an empty string.

--- exercises/practice/reverse-string/ReverseString.vb
+++ exercises/practice/reverse-string/ReverseString.vb
@@ -1,5 +1,10 @@
 Public Module ReverseString
     Public Function Reverse(ByVal inputString As String) As String
-        Throw New NotImplementedException("Delete this statement and write your own implementation here")
+        Const cstrEmpty As String = ""
+        If (inputString.Length = 0) Then
+            Return cstrEmpty
+        Else
+            Throw New NotImplementedException("Delete this statement and write your own implementation here")
+        End If
     End Function
 End Module
