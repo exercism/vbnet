0e143c79aee3b6a55dd7118ce02ab84a01d8a4b2
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 17:27:00 +0200

    Add nucleotide-count exercise (#241)

---
+++ exercises/practice/nucleotide-count/NucleotideCount.vb
@@ -0,0 +1,8 @@
+Imports System
+Imports System.Collections.Generic
+
+Public Module NucleotideCount
+    Public Function Count(ByVal sequence As String) As IDictionary(Of Char, Integer)
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+End Module
