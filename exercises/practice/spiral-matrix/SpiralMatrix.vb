f72f3d194e098718249994a21cb9fe4f38e6b808
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 15:44:44 +0200

    Add spiral-matrix exercise

---
+++ exercises/practice/spiral-matrix/SpiralMatrix.vb
@@ -0,0 +1,7 @@
+Imports System
+
+Public Class SpiralMatrix
+    Public Shared Function GetMatrix(ByVal size As Integer) As Integer(,)
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+End Class
