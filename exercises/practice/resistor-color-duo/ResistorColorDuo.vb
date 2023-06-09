0f2ae50361f727f7072556d7840001f858e3d074
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 17:45:02 +0200

    Add resistor-color-duo exercise (#260)

---
+++ exercises/practice/resistor-color-duo/ResistorColorDuo.vb
@@ -0,0 +1,7 @@
+Imports System
+
+Public Module ResistorColorDuo
+    Public Function Value(ByVal colors As String()) As Integer
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+End Module
