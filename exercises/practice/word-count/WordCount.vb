febc0f69c46affe1ba3c98ee7b961d4475253aae
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 16:09:44 +0200

    Add word-count exercise

---
+++ exercises/practice/word-count/WordCount.vb
@@ -0,0 +1,8 @@
+Imports System
+Imports System.Collections.Generic
+
+Public Module WordCount
+    Public Function CountWords(ByVal phrase As String) As IDictionary(Of String, Integer)
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+End Module
