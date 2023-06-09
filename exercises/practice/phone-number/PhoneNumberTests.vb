f27fe3f7b62f91393b03744f990d0465d045d17c
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 14:01:31 +0200

    Separate tests with a blank line (#194)

--- exercises/practice/phone-number/PhoneNumberTests.vb
+++ exercises/practice/phone-number/PhoneNumberTests.vb
@@ -6,86 +6,103 @@ Public Class PhoneNumberTests
         Dim phrase = "(223) 456-7890"
         Assert.Equal("2234567890", PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub CleansNumbersWithDots()
         Dim phrase = "223.456.7890"
         Assert.Equal("2234567890", PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub CleansNumbersWithMultipleSpaces()
         Dim phrase = "223 456   7890   "
         Assert.Equal("2234567890", PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub InvalidWhen9Digits()
         Dim phrase = "123456789"
         Assert.Throws(Of ArgumentException)(Function() PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub InvalidWhen11DigitsDoesNotStartWithA1()
         Dim phrase = "22234567890"
         Assert.Throws(Of ArgumentException)(Function() PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub ValidWhen11DigitsAndStartingWith1()
         Dim phrase = "12234567890"
         Assert.Equal("2234567890", PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub ValidWhen11DigitsAndStartingWith1EvenWithPunctuation()
         Dim phrase = "+1 (223) 456-7890"
         Assert.Equal("2234567890", PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub InvalidWhenMoreThan11Digits()
         Dim phrase = "321234567890"
         Assert.Throws(Of ArgumentException)(Function() PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub InvalidWithLetters()
         Dim phrase = "123-abc-7890"
         Assert.Throws(Of ArgumentException)(Function() PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub InvalidWithPunctuations()
         Dim phrase = "123-@:!-7890"
         Assert.Throws(Of ArgumentException)(Function() PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub InvalidIfAreaCodeStartsWith0()
         Dim phrase = "(023) 456-7890"
         Assert.Throws(Of ArgumentException)(Function() PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub InvalidIfAreaCodeStartsWith1()
         Dim phrase = "(123) 456-7890"
         Assert.Throws(Of ArgumentException)(Function() PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub InvalidIfExchangeCodeStartsWith0()
         Dim phrase = "(223) 056-7890"
         Assert.Throws(Of ArgumentException)(Function() PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub InvalidIfExchangeCodeStartsWith1()
         Dim phrase = "(223) 156-7890"
         Assert.Throws(Of ArgumentException)(Function() PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub InvalidIfAreaCodeStartsWith0OnValid11DigitNumber()
         Dim phrase = "1 (023) 456-7890"
         Assert.Throws(Of ArgumentException)(Function() PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub InvalidIfAreaCodeStartsWith1OnValid11DigitNumber()
         Dim phrase = "1 (123) 456-7890"
         Assert.Throws(Of ArgumentException)(Function() PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub InvalidIfExchangeCodeStartsWith0OnValid11DigitNumber()
         Dim phrase = "1 (223) 056-7890"
         Assert.Throws(Of ArgumentException)(Function() PhoneNumber.Clean(phrase))
     End Sub
+
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub InvalidIfExchangeCodeStartsWith1OnValid11DigitNumber()
         Dim phrase = "1 (223) 156-7890"
--- exercises/practice/two-fer/TwoFerTests.vb
+++ exercises/practice/two-fer/TwoFerTests.vb
@@ -4,10 +4,12 @@ Public Class TwoFerTests
     Public Sub NoNameGiven()
         Assert.Equal("One for you, one for me.", TwoFer.Speak())
     End Sub
+
     <Fact>
     Public Sub ANameGiven()
         Assert.Equal("One for Alice, one for me.", TwoFer.Speak("Alice"))
     End Sub
+
     <Fact>
     Public Sub AnotherNameGiven()
         Assert.Equal("One for Bob, one for me.", TwoFer.Speak("Bob"))
