4387f0d0d4c86ca4aa55059cc864dc7e65a40084
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 17:55:05 +0200

    Add bank-account exercise (#199)

---
+++ exercises/practice/bank-account/BankAccount.vb
@@ -0,0 +1,21 @@
+Imports System
+
+Public Class BankAccount
+    Public Sub OpenMethod()
+        Throw New NotImplementedException("You need to implement this function.")
+    End Sub
+
+    Public Sub CloseMethod()
+        Throw New NotImplementedException("You need to implement this function.")
+    End Sub
+
+    Public ReadOnly Property BalanceProp As Decimal
+        Get
+            Throw New NotImplementedException("You need to implement this property.")
+        End Get
+    End Property
+
+    Public Sub UpdateBalanceMethod(ByVal change As Decimal)
+        Throw New NotImplementedException("You need to implement this function.")
+    End Sub
+End Class
