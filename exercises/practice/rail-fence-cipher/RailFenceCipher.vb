26ced3d8fca1b8e8f542dd32e0890c67f5a3d048
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 17:18:06 +0200

    Add rail-fence-cipher exercise (#255)

---
+++ exercises/practice/rail-fence-cipher/RailFenceCipher.vb
@@ -0,0 +1,14 @@
+Imports System
+
+Public Class RailFenceCipher
+    Public Sub New(ByVal rails As Integer)
+    End Sub
+
+    Public Function Encode(ByVal input As String) As String
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+
+    Public Function Decode(ByVal input As String) As String
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+End Class
