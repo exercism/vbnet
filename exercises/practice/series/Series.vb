56552e7d82fef27cbd780f69d2b5f447d4987dec
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 15:48:16 +0200

    Add series exercise (#274)

---
+++ exercises/practice/series/Series.vb
@@ -0,0 +1,7 @@
+Imports System
+
+Public Module Series
+    Public Function Slices(ByVal numbers As String, ByVal sliceLength As Integer) As String()
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+End Module
