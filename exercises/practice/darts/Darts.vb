81becffef8970b016a728e1b509960a286047920
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 21:55:35 +0200

    Add darts exercise (#210)

---
+++ exercises/practice/darts/Darts.vb
@@ -0,0 +1,7 @@
+Imports System
+
+Public Module Darts
+    Public Function Score(ByVal x As Double, ByVal y As Double) As Integer
+        Throw New NotImplementedException("You need to implement this function")
+    End Function
+End Module
