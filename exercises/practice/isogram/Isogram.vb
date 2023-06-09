eeef7998c0411341f2454547c6f73ec7d2b0baff
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 17:49:41 +0200

    Add isogram exercise (#229)

---
+++ exercises/practice/isogram/Isogram.vb
@@ -0,0 +1,7 @@
+Imports System
+
+Public Module Isogram
+    Public Function IsIsogram(ByVal word As String) As Boolean
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+End Module
