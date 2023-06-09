ee5b58407d80ce9c665e615d71b18ecdd93f739d
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 14:03:55 +0200

    two-fer: add missing skips (#193)

--- exercises/practice/two-fer/TwoFerTests.vb
+++ exercises/practice/two-fer/TwoFerTests.vb
@@ -5,12 +5,12 @@ Public Class TwoFerTests
         Assert.Equal("One for you, one for me.", TwoFer.Speak())
     End Sub
 
-    <Fact>
+    <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub ANameGiven()
         Assert.Equal("One for Alice, one for me.", TwoFer.Speak("Alice"))
     End Sub
-
-    <Fact>
+  
+    <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub AnotherNameGiven()
         Assert.Equal("One for Bob, one for me.", TwoFer.Speak("Bob"))
     End Sub
