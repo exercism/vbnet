9505cc211957d8ed6120ca3124885dd6abd10fae
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 20:05:30 +0200

    Add difference-of-squares exercise

---
+++ exercises/practice/difference-of-squares/DifferenceOfSquares.vb
@@ -0,0 +1,15 @@
+Imports System
+
+Public Module DifferenceOfSquares
+    Public Function CalculateSquareOfSum(ByVal max As Integer) As Integer
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+
+    Public Function CalculateSumOfSquares(ByVal max As Integer) As Integer
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+
+    Public Function CalculateDifferenceOfSquares(ByVal max As Integer) As Integer
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+End Module
