d6268552bb2f03aedf003587cd7094da08ac8a1c
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 17:51:04 +0200

    Add grains exercise (#224)

---
+++ exercises/practice/grains/Grains.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Public Module Grains
+    Public Function Square(ByVal n As Integer) As ULong
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+
+    Public Function Total() As ULong
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+End Module
