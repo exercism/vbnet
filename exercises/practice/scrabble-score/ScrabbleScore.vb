aaf08fb5283e5b0db6eb0f6931ca68c6a024236f
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 16:19:21 +0200

    Add scrabble-score exercise (#272)

---
+++ exercises/practice/scrabble-score/ScrabbleScore.vb
@@ -0,0 +1,7 @@
+Imports System
+
+Public Module ScrabbleScore
+    Public Function Score(ByVal input As String) As Integer
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+End Module
