f32616e5ed6bf13b98bcef4db75e6750d567cd83
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-09 17:53:48 +0200

    Add etl exercise (#192)

---
+++ exercises/practice/etl/Etl.vb
@@ -0,0 +1,8 @@
+Imports System
+Imports System.Collections.Generic
+
+Public Module Etl
+    Public Function Transform(ByVal old As Dictionary(Of Integer, String())) As Dictionary(Of String, Integer)
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+End Module
