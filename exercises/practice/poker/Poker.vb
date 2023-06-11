1ecc415211c890851c78fcc974fa60bb0f20d228
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-11 13:07:43 +0200

    Add poker exercise (#248)

---
+++ exercises/practice/poker/Poker.vb
@@ -0,0 +1,8 @@
+Imports System
+Imports System.Collections.Generic
+
+Public Module Poker
+    Public Function BestHands(ByVal hands As IEnumerable(Of String)) As IEnumerable(Of String)
+        Throw New NotImplementedException()
+    End Function
+End Module
