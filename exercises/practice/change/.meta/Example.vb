145247ec2940a654c29035794fc2df23932e626d
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-12 13:02:03 +0200

    Fix

--- exercises/practice/change/.meta/Example.vb
+++ exercises/practice/change/.meta/Example.vb
@@ -2,35 +2,33 @@ Imports System
 Imports System.Collections.Generic
 Imports System.Linq
 
-Module Change
-    Function FindFewestCoins(ByVal coins As Integer(), ByVal target As Integer) As Integer()
-        If target < 0 Then
-            Throw New ArgumentException("Target amount cannot be negative.")
+Public Module Change
+    Public Function FindFewestCoins(ByVal coins As Integer(), ByVal target As Integer) As Integer()
+        If target = 0 Then
+            Return New List(Of Integer)().ToArray()
         End If
 
-        If target > 0 AndAlso target < coins.Min() Then
-            Throw New ArgumentException("Target amount cannot be less than minimal coin value.")
+        If target < 0 OrElse target < coins.Min() Then
+            Throw New ArgumentException()
         End If
 
-        Dim minimalCoinsMapSeed = New Dictionary(Of Integer, List(Of Integer))()
-        minimalCoinsMapSeed(0) = New List(Of Integer)(0)
-        Dim minimalCoinsMap = Enumerable.Range(1, target).Aggregate(minimalCoinsMapSeed, Function(current, subTarget) UpdateMinimalCoinsMap(coins, current, subTarget))
-        Dim minimalCoins As List = Nothing
-        If minimalCoinsMap.TryGetValue(target, minimalCoins) Then Return minimalCoins.OrderBy(Function(x) x).ToArray()
+        Dim fewestCoins = New Integer(target + 1 - 1)() {}
+        fewestCoins(0) = Array.Empty(Of Integer)()
+
+        For amount = 1 To target
+            Dim change As IEnumerable(Of Integer) = coins.Where(Function(coin) coin <= amount).[Select](Function(coin) fewestCoins(amount - coin).Append(coin)).OrderBy(Function(x) x.Count()).FirstOrDefault(Function(y) y.Sum() = amount)
+
+            If change Is Nothing Then
+                fewestCoins(amount) = New List(Of Integer)().ToArray()
+            Else
+                fewestCoins(amount) = change.OrderBy(Function(coin) coin).ToArray()
+            End If
+        Next
+
+        If fewestCoins(target).Count() > 0 Then
+            Return fewestCoins(target)
+        End If
+        
         Throw New ArgumentException()
-    End Function
-
-    Private Function UpdateMinimalCoinsMap(ByVal coins As Integer(), ByVal current As Dictionary(Of Integer, List(Of Integer)), ByVal subTarget As Integer) As Dictionary(Of Integer, List(Of Integer))
-        Dim subTargetMinimalCoins = MinimalCoins(coins, current, subTarget)
-        If subTargetMinimalCoins IsNot Nothing Then current.Add(subTarget, subTargetMinimalCoins)
-        Return current
-    End Function
-
-    Private Function MinimalCoins(ByVal coins As Integer(), ByVal current As Dictionary(Of Integer, List(Of Integer)), ByVal subTarget As Integer) As List(Of Integer)
-        Dim subTargetMinimalCoins As List = Nothing
-        Return coins.Where(Function(coin) coin <= subTarget).[Select](Function(coin)
-                                                                          Dim subTargetMinimalCoins As List = Nothing
-                                                                          Return If(current.TryGetValue(subTarget - coin, subTargetMinimalCoins), subTargetMinimalCoins.Append(coin).ToList(), Nothing)
-                                                                      End Function).Where(Function(subTargetMinimalCoins) subTargetMinimalCoins IsNot Nothing).OrderBy(Function(subTargetMinimalCoins) subTargetMinimalCoins.Count).FirstOrDefault()
     End Function
 End Module
