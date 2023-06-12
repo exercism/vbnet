4066994cfa2bb5644203da735c75cac4091ab386
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-12 12:34:33 +0200

    Simplify

--- exercises/practice/robot-simulator/.meta/Example.vb
+++ exercises/practice/robot-simulator/.meta/Example.vb
@@ -8,42 +8,17 @@ Public Enum DirectionType
 End Enum
 
 Public Class RobotSimulator
-
-    Private _DirectionProp As DirectionType, _XProp As Integer, _YProp As Integer
     Public Sub New(ByVal bearing As DirectionType, ByVal x As Integer, ByVal y As Integer)
-        _DirectionProp = bearing
-        _XProp = x
-        _YProp = y
+        Direction = bearing
+        X = x
+        Y = y
     End Sub
 
     Public Property Direction As DirectionType
-        Get
-            Return _DirectionProp
-        End Get
-        Private Set(ByVal value As DirectionType)
-            _DirectionProp = value
-        End Set
-    End Property
-
     Public Property X As Integer
-        Get
-            Return _XProp
-        End Get
-        Private Set(ByVal value As Integer)
-            _XProp = value
-        End Set
-    End Property
-
     Public Property Y As Integer
-        Get
-            Return _YProp
-        End Get
-        Private Set(ByVal value As Integer)
-            _YProp = value
-        End Set
-    End Property
 
-    Public Sub MoveMethod(ByVal instructions As String)
+    Public Sub Move(ByVal instructions As String)
         For Each instruction In instructions
             Move(instruction)
         Next
@@ -63,45 +38,45 @@ Public Class RobotSimulator
     End Sub
 
     Private Sub TurnLeft()
-        Select Case _DirectionProp
+        Select Case Direction
             Case DirectionType.North
-                _DirectionProp = DirectionType.West
+                Direction = DirectionType.West
             Case DirectionType.East
-                _DirectionProp = DirectionType.North
+                Direction = DirectionType.North
             Case DirectionType.South
-                _DirectionProp = DirectionType.East
+                Direction = DirectionType.East
             Case DirectionType.West
-                _DirectionProp = DirectionType.South
+                Direction = DirectionType.South
             Case Else
                 Throw New ArgumentOutOfRangeException()
         End Select
     End Sub
 
     Private Sub TurnRight()
-        Select Case _DirectionProp
+        Select Case Direction
             Case DirectionType.North
-                _DirectionProp = DirectionType.East
+                Direction = DirectionType.East
             Case DirectionType.East
-                _DirectionProp = DirectionType.South
+                Direction = DirectionType.South
             Case DirectionType.South
-                _DirectionProp = DirectionType.West
+                Direction = DirectionType.West
             Case DirectionType.West
-                _DirectionProp = DirectionType.North
+                Direction = DirectionType.North
             Case Else
                 Throw New ArgumentOutOfRangeException()
         End Select
     End Sub
 
     Private Sub Advance()
-        Select Case _DirectionProp
+        Select Case Direction
             Case DirectionType.North
-                _YProp += 1
+                Y += 1
             Case DirectionType.East
-                _XProp += 1
+                X += 1
             Case DirectionType.South
-                _YProp -= 1
+                Y -= 1
             Case DirectionType.West
-                _XProp -= 1
+                X -= 1
             Case Else
                 Throw New ArgumentOutOfRangeException()
         End Select
