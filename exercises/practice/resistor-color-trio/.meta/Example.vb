8f5dd169b03cb73e926a22690e4417e596a4ba1e
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-12 13:48:19 +0200

    Apply suggestions from code review
    Co-authored-by: Bob Hoeppner <32035397+bobahop@users.noreply.github.com>

--- exercises/practice/resistor-color-trio/.meta/Example.vb
+++ exercises/practice/resistor-color-trio/.meta/Example.vb
@@ -3,8 +3,12 @@ Imports System
 Public Module ResistorColorTrio
     Private Const KiloOhms As Integer = 1_000
     Private ReadOnly AllColors As String() = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}
-    Public Function LabelMethod(ByVal colors As String()) As String
-        Return $"{(If(CSharpImpl.__Assign(_value, TryCast(GetValue(colors), Integer)) IsNot Nothing AndAlso _value >= KiloOhms, _value / KiloOhms, _value))} " & ResistorColorTrio.GetUnit(_value)
+    Public Function Label(ByVal colors As String()) As String
+        Dim value = GetValue(colors)
+        Dim unit = GetUnit(value)
+        If unit = "kiloohms" Then value \= 1_000
+        Return String.Format("{0} {1}", value, unit)
+    End Function    
     End Function
     Private Function GetValue(ByVal colors As String()) As Integer
         Return (ResistorValue(colors(0)) * 10 + ResistorValue(colors(1))) * CInt(Math.Pow(10, ResistorValue(colors(2))))
@@ -16,11 +20,5 @@ Public Module ResistorColorTrio
         Return Array.IndexOf(AllColors, color)
     End Function
 
-    Private Class CSharpImpl
-        <Obsolete("Please refactor calling code to use normal Visual Basic assignment")>
-        Shared Function __Assign(Of T)(ByRef target As T, value As T) As T
-            target = value
-            Return value
-        End Function
     End Class
 End Module
