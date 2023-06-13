4610e643d92f0bc1756e211bb59810992ccec37b
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-13 10:11:23 +0200

    Apply suggestions from code review
    Co-authored-by: Bob Hoeppner <32035397+bobahop@users.noreply.github.com>

--- exercises/practice/grade-school/.meta/Example.vb
+++ exercises/practice/grade-school/.meta/Example.vb
@@ -1,16 +1,8 @@
 Imports System.Collections.Generic
 
 Public Class GradeSchool
-        ''' Cannot convert FieldDeclarationSyntax, System.InvalidCastException: Unable to cast object of type 'Microsoft.CodeAnalysis.VisualBasic.Syntax.EmptyStatementSyntax' to type 'Microsoft.CodeAnalysis.VisualBasic.Syntax.ExpressionSyntax'.
-'''    at ICSharpCode.CodeConverter.VB.CommonConversions.RemodelVariableDeclaration(VariableDeclarationSyntax declaration) in /_/CodeConverter/VB/CommonConversions.cs:line 474
-'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitFieldDeclaration(FieldDeclarationSyntax node) in /_/CodeConverter/VB/NodesVisitor.cs:line 326
-'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
-'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping) in /_/CodeConverter/VB/CommentConvertingVisitorWrapper.cs:line 20
-''' 
-''' Input:
-'''     private readonly System.Collections.Generic.SortedDictionary<int, System.Collections.Generic.List<string>> _roster = new();
+    Private ReadOnly _roster As New SortedDictionary(Of Integer, List(Of String))
 
-''' 
 
     Public Function Add(ByVal student As String, ByVal grade As Integer) As Boolean
         If Me._roster.Values.Any(Function(students) students.Contains(student)) Then Return False
@@ -30,6 +22,7 @@ Public Class GradeSchool
     End Function
 
     Public Function Grade(ByVal pGrade As Integer) As IEnumerable(Of String)
+        Dim students As New List(Of String)()    
         Return If(Me._roster.TryGetValue(pGrade, students), students, New List(Of String)())
     End Function
 End Class
