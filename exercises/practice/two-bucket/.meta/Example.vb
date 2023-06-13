4601dfaba904f2d92ea0a662bdf8d91e726a5aac
Bob Hoeppner <32035397+bobahop@users.noreply.github.com>
2023-06-13 08:10:08 -0500

    Update Example.vb

--- exercises/practice/two-bucket/.meta/Example.vb
+++ exercises/practice/two-bucket/.meta/Example.vb
@@ -1,6 +1,4 @@
 Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 
 Public Enum Bucket
     One
@@ -11,62 +9,97 @@ Public Class TwoBucketResult
     Public Property Moves As Integer
     Public Property GoalBucket As Bucket
     Public Property OtherBucket As Integer
+
+    Public Sub New (moves as Integer, goalBucket as Bucket, otherbucket as Integer)
+        Me.Moves = moves
+        Me.GoalBucket = goalBucket
+        Me.Otherbucket = otherbucket
+    End Sub
 End Class
 
 Public Class TwoBucket
-    Private sizes As Integer()
-    Private startBucket As Integer
-    Public Sub New(ByVal bucketOne As Integer, ByVal bucketTwo As Integer, ByVal startBucket As Bucket)
-        sizes = {bucketOne, bucketTwo}
-        Me.startBucket = startBucket
-    End Sub
 
-    Private Function Empty(ByVal _buckets As Integer(), ByVal i As Integer) As Integer()
-        Return If(i = 0, {0, _buckets(1)}, {_buckets(0), 0})
-    End Function
+    Private Class Pail
+        Public Property Name As Bucket
+        Public Property Size As Integer
+        Public Property Level As Integer = 0
 
-    Private Function Fill(ByVal _buckets As Integer(), ByVal i As Integer) As Integer()
-        Return If(i = 0, {sizes(0), _buckets(1)}, {_buckets(0), sizes(1)})
-    End Function
+        Public Sub New(name As Bucket, size As Integer)
+            Me.Name = name
+            Me.Size = size
+        End Sub
 
-    Private Function Consolidate(ByVal _buckets As Integer(), ByVal i As Integer) As Integer()
-        Dim amount = {_buckets(1 - i), sizes(i) - _buckets(i)}.Min()
-        Dim target = _buckets(i) + amount
-        Dim src = _buckets(1 - i) - amount
-        Return If(i = 0, {target, src}, {src, target})
-    End Function
+        Public Function Room() As Integer
+            Return Size - Level
+        End Function
 
-    Public Function Measure(ByVal goal As Integer) As TwoBucketResult
-        Dim invalid = {0, 0}
-        invalid(1 - startBucket) = sizes(1 - startBucket)
-        Dim invalidStr = String.Join(",", invalid)
-        Dim buckets = {0, 0}
-        buckets(startBucket) = sizes(startBucket)
-        Dim toVisit = New Queue(Of (Integer(), Integer))()
-        Dim visited = New HashSet(Of String)()
-        Dim count = 1
-        Dim goalBucket = Array.IndexOf(buckets, goal)
-        While goalBucket < 0
-            Dim key = String.Join(",", buckets)
-            If Not visited.Contains(key) AndAlso Not key.Equals(invalidStr) Then
-                visited.Add(key)
-                Dim nc = count + 1
-                For i = 0 To 1
-                    If buckets(i) <> 0 Then toVisit.Enqueue((Empty(buckets, i), nc))
-                    If buckets(i) <> sizes(i) Then
-                        toVisit.Enqueue((Fill(buckets, i), nc))
-                        toVisit.Enqueue((Consolidate(buckets, i), nc))
-                    End If
-                Next
+        Public ReadOnly Property IsFull As Integer
+            Get
+                Return Level = Size
+            End Get
+        End Property
+
+        Public ReadOnly Property IsEmpty As Integer
+            Get
+                Return Level = 0
+            End Get
+        End Property
+
+        Public Sub Fill()
+            Level = Size
+        End Sub
+
+        Public Sub Empty()
+            Level = 0
+        End Sub
+
+        Public Sub PourInto(Other as Pail)
+            Dim amount = Math.Min(Level, Other.Room)
+            Level -= amount
+            Other.Level += amount
+        End Sub
+    End Class
+
+    Private ReadOnly start As Pail
+    Private ReadOnly other As Pail
+
+    Public Sub New(ByVal bucketOneCapacity As Integer, ByVal bucketTwoCapacity As Integer, ByVal startBucket As Bucket)
+        If startBucket = Bucket.One Then
+            start = New Pail(Bucket.One, bucketOneCapacity)
+            other = New Pail(Bucket.Two, bucketTwoCapacity)
+        Else
+            start = New Pail(Bucket.Two, bucketTwoCapacity)
+            other = New Pail(Bucket.One, bucketOneCapacity)
+        End If
+        End Sub
+
+
+        Public Function Measure(goal As Integer) As TwoBucketResult
+        If goal > Math.Max(start.Size, other.Size) Then Throw New ArgumentException(goal)
+
+        If other.Size = goal Then Return New TwoBucketResult(2, other.Name, start.Size)
+
+            Dim moves = 0
+
+            While start.Level <> goal AndAlso other.Level <> goal
+            If start.IsEmpty Then
+                start.Fill()
+            ElseIf other.IsFull Then
+                other.Empty()
+            Else
+                start.PourInto(other)
             End If
-            If Not toVisit.Any() Then Throw New ArgumentException("no more moves!")
-            (buckets, count) = toVisit.Dequeue()
-            goalBucket = Array.IndexOf(buckets, goal)
-        End While
-        Return New TwoBucketResult With {
-    .Moves = count,
-    .GoalBucket = goalBucket,
-    .OtherBucket = buckets(1 - goalBucket)
-}
-    End Function
-End Class
+
+            If start.IsFull AndAlso other.IsFull Then Throw New ArgumentException(goal)
+
+            moves += 1
+            End While
+
+        If start.Level = goal Then
+            Return New TwoBucketResult(moves, start.Name, other.Level)
+        Else
+            Return New TwoBucketResult(moves, other.Name, start.Level)
+        End If
+            
+        End Function
+    End Class
