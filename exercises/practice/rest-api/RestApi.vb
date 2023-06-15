5cce023cecee1ff3619cb0b712ace22524949d0c
Bob Hoeppner <32035397+bobahop@users.noreply.github.com>
2023-06-14 19:10:05 -0500

    Update RestApi.vb
    Got rid of ByVals

--- exercises/practice/rest-api/RestApi.vb
+++ exercises/practice/rest-api/RestApi.vb
@@ -1,15 +1,15 @@
 Imports System
 
 Public Class RestApi
-    Public Sub New(ByVal database As String)
+    Public Sub New(database As String)
         Throw New NotImplementedException("You need to implement this function.")
     End Sub
 
-    Public Function [Get](ByVal url As String, ByVal Optional payload As String = Nothing) As String
+    Public Function [Get](url As String, Optional payload As String = Nothing) As String
         Throw New NotImplementedException("You need to implement this function.")
     End Function
 
-    Public Function Post(ByVal url As String, ByVal payload As String) As String
+    Public Function Post(url As String, payload As String) As String
         Throw New NotImplementedException("You need to implement this function.")
     End Function
 End Class
