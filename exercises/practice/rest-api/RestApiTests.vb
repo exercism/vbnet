68912822d2d3bb1fb377dfe020bf7d5dc01ac4e3
Bob Hoeppner <32035397+bobahop@users.noreply.github.com>
2023-06-14 19:12:52 -0500

    Update RestApiTests.vb
    Names conformed to naming conventions

--- exercises/practice/rest-api/RestApiTests.vb
+++ exercises/practice/rest-api/RestApiTests.vb
@@ -11,91 +11,91 @@ Public Class RestApiTests
         Assert.Equal(expected, actual)
     End Sub
 
-    <Fact(Skip:="Remove this Skip property to run this test")>
+    <Fact>
     Public Sub Add_user()
         Dim url = "/add"
         Dim payload = "{""user"":""Adam""}"
         Dim database = "[]"
         Dim sut = New RestApi(database)
         Dim actual = sut.Post(url, payload)
-        Dim expected = "{""name"":""Adam"",""owes"":{},""owed_by"":{},""balance"":0}"
+        Dim expected = "{""Name"":""Adam"",""Owes"":{},""OwedBy"":{},""Balance"":0}"
         Assert.Equal(expected, actual)
     End Sub
 
-    <Fact(Skip:="Remove this Skip property to run this test")>
+    <Fact>
     Public Sub Get_single_user()
         Dim url = "/users"
         Dim payload = "{""users"":[""Bob""]}"
-        Dim database = "[{""name"":""Adam"",""owes"":{},""owed_by"":{},""balance"":0},{""name"":""Bob"",""owes"":{},""owed_by"":{},""balance"":0}]"
+        Dim database = "[{""Name"":""Adam"",""Owes"":{},""OwedBy"":{},""Balance"":0},{""Name"":""Bob"",""Owes"":{},""OwedBy"":{},""Balance"":0}]"
         Dim sut = New RestApi(database)
         Dim actual = sut.Get(url, payload)
-        Dim expected = "[{""name"":""Bob"",""owes"":{},""owed_by"":{},""balance"":0}]"
+        Dim expected = "[{""Name"":""Bob"",""Owes"":{},""OwedBy"":{},""Balance"":0}]"
         Assert.Equal(expected, actual)
     End Sub
 
-    <Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub Both_users_have_0_balance()
+    <Fact>
+    Public Sub Both_users_have_0_Balance()
         Dim url = "/iou"
         Dim payload = "{""lender"":""Adam"",""borrower"":""Bob"",""amount"":3}"
-        Dim database = "[{""name"":""Adam"",""owes"":{},""owed_by"":{},""balance"":0},{""name"":""Bob"",""owes"":{},""owed_by"":{},""balance"":0}]"
+        Dim database = "[{""Name"":""Adam"",""Owes"":{},""OwedBy"":{},""Balance"":0},{""Name"":""Bob"",""Owes"":{},""OwedBy"":{},""Balance"":0}]"
         Dim sut = New RestApi(database)
         Dim actual = sut.Post(url, payload)
-        Dim expected = "[{""name"":""Adam"",""owes"":{},""owed_by"":{""Bob"":3},""balance"":3},{""name"":""Bob"",""owes"":{""Adam"":3},""owed_by"":{},""balance"":-3}]"
+        Dim expected = "[{""Name"":""Adam"",""Owes"":{},""OwedBy"":{""Bob"":3},""Balance"":3},{""Name"":""Bob"",""Owes"":{""Adam"":3},""OwedBy"":{},""Balance"":-3}]"
         Assert.Equal(expected, actual)
     End Sub
 
-    <Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub Borrower_has_negative_balance()
+    <Fact>
+    Public Sub Borrower_has_negative_Balance()
         Dim url = "/iou"
         Dim payload = "{""lender"":""Adam"",""borrower"":""Bob"",""amount"":3}"
-        Dim database = "[{""name"":""Adam"",""owes"":{},""owed_by"":{},""balance"":0},{""name"":""Bob"",""owes"":{""Chuck"":3},""owed_by"":{},""balance"":-3},{""name"":""Chuck"",""owes"":{},""owed_by"":{""Bob"":3},""balance"":3}]"
+        Dim database = "[{""Name"":""Adam"",""Owes"":{},""OwedBy"":{},""Balance"":0},{""Name"":""Bob"",""Owes"":{""Chuck"":3},""OwedBy"":{},""Balance"":-3},{""Name"":""Chuck"",""Owes"":{},""OwedBy"":{""Bob"":3},""Balance"":3}]"
         Dim sut = New RestApi(database)
         Dim actual = sut.Post(url, payload)
-        Dim expected = "[{""name"":""Adam"",""owes"":{},""owed_by"":{""Bob"":3},""balance"":3},{""name"":""Bob"",""owes"":{""Adam"":3,""Chuck"":3},""owed_by"":{},""balance"":-6}]"
+        Dim expected = "[{""Name"":""Adam"",""Owes"":{},""OwedBy"":{""Bob"":3},""Balance"":3},{""Name"":""Bob"",""Owes"":{""Adam"":3,""Chuck"":3},""OwedBy"":{},""Balance"":-6}]"
         Assert.Equal(expected, actual)
     End Sub
 
-    <Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub Lender_has_negative_balance()
+    <Fact>
+    Public Sub Lender_has_negative_Balance()
         Dim url = "/iou"
         Dim payload = "{""lender"":""Bob"",""borrower"":""Adam"",""amount"":3}"
-        Dim database = "[{""name"":""Adam"",""owes"":{},""owed_by"":{},""balance"":0},{""name"":""Bob"",""owes"":{""Chuck"":3},""owed_by"":{},""balance"":-3},{""name"":""Chuck"",""owes"":{},""owed_by"":{""Bob"":3},""balance"":3}]"
+        Dim database = "[{""Name"":""Adam"",""Owes"":{},""OwedBy"":{},""Balance"":0},{""Name"":""Bob"",""Owes"":{""Chuck"":3},""OwedBy"":{},""Balance"":-3},{""Name"":""Chuck"",""Owes"":{},""OwedBy"":{""Bob"":3},""Balance"":3}]"
         Dim sut = New RestApi(database)
         Dim actual = sut.Post(url, payload)
-        Dim expected = "[{""name"":""Adam"",""owes"":{""Bob"":3},""owed_by"":{},""balance"":-3},{""name"":""Bob"",""owes"":{""Chuck"":3},""owed_by"":{""Adam"":3},""balance"":0}]"
+        Dim expected = "[{""Name"":""Adam"",""Owes"":{""Bob"":3},""OwedBy"":{},""Balance"":-3},{""Name"":""Bob"",""Owes"":{""Chuck"":3},""OwedBy"":{""Adam"":3},""Balance"":0}]"
         Assert.Equal(expected, actual)
     End Sub
 
-    <Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub Lender_owes_borrower()
+    <Fact>
+    Public Sub Lender_Owes_borrower()
         Dim url = "/iou"
         Dim payload = "{""lender"":""Adam"",""borrower"":""Bob"",""amount"":2}"
-        Dim database = "[{""name"":""Adam"",""owes"":{""Bob"":3},""owed_by"":{},""balance"":-3},{""name"":""Bob"",""owes"":{},""owed_by"":{""Adam"":3},""balance"":3}]"
+        Dim database = "[{""Name"":""Adam"",""Owes"":{""Bob"":3},""OwedBy"":{},""Balance"":-3},{""Name"":""Bob"",""Owes"":{},""OwedBy"":{""Adam"":3},""Balance"":3}]"
         Dim sut = New RestApi(database)
         Dim actual = sut.Post(url, payload)
-        Dim expected = "[{""name"":""Adam"",""owes"":{""Bob"":1},""owed_by"":{},""balance"":-1},{""name"":""Bob"",""owes"":{},""owed_by"":{""Adam"":1},""balance"":1}]"
+        Dim expected = "[{""Name"":""Adam"",""Owes"":{""Bob"":1},""OwedBy"":{},""Balance"":-1},{""Name"":""Bob"",""Owes"":{},""OwedBy"":{""Adam"":1},""Balance"":1}]"
         Assert.Equal(expected, actual)
     End Sub
 
-    <Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub Lender_owes_borrower_less_than_new_loan()
+    <Fact>
+    Public Sub Lender_Owes_borrower_less_than_new_loan()
         Dim url = "/iou"
         Dim payload = "{""lender"":""Adam"",""borrower"":""Bob"",""amount"":4}"
-        Dim database = "[{""name"":""Adam"",""owes"":{""Bob"":3},""owed_by"":{},""balance"":-3},{""name"":""Bob"",""owes"":{},""owed_by"":{""Adam"":3},""balance"":3}]"
+        Dim database = "[{""Name"":""Adam"",""Owes"":{""Bob"":3},""OwedBy"":{},""Balance"":-3},{""Name"":""Bob"",""Owes"":{},""OwedBy"":{""Adam"":3},""Balance"":3}]"
         Dim sut = New RestApi(database)
         Dim actual = sut.Post(url, payload)
-        Dim expected = "[{""name"":""Adam"",""owes"":{},""owed_by"":{""Bob"":1},""balance"":1},{""name"":""Bob"",""owes"":{""Adam"":1},""owed_by"":{},""balance"":-1}]"
+        Dim expected = "[{""Name"":""Adam"",""Owes"":{},""OwedBy"":{""Bob"":1},""Balance"":1},{""Name"":""Bob"",""Owes"":{""Adam"":1},""OwedBy"":{},""Balance"":-1}]"
         Assert.Equal(expected, actual)
     End Sub
 
-    <Fact(Skip:="Remove this Skip property to run this test")>
-    Public Sub Lender_owes_borrower_same_as_new_loan()
+    <Fact>
+    Public Sub Lender_Owes_borrower_same_as_new_loan()
         Dim url = "/iou"
         Dim payload = "{""lender"":""Adam"",""borrower"":""Bob"",""amount"":3}"
-        Dim database = "[{""name"":""Adam"",""owes"":{""Bob"":3},""owed_by"":{},""balance"":-3},{""name"":""Bob"",""owes"":{},""owed_by"":{""Adam"":3},""balance"":3}]"
+        Dim database = "[{""Name"":""Adam"",""Owes"":{""Bob"":3},""OwedBy"":{},""Balance"":-3},{""Name"":""Bob"",""Owes"":{},""OwedBy"":{""Adam"":3},""Balance"":3}]"
         Dim sut = New RestApi(database)
         Dim actual = sut.Post(url, payload)
-        Dim expected = "[{""name"":""Adam"",""owes"":{},""owed_by"":{},""balance"":0},{""name"":""Bob"",""owes"":{},""owed_by"":{},""balance"":0}]"
+        Dim expected = "[{""Name"":""Adam"",""Owes"":{},""OwedBy"":{},""Balance"":0},{""Name"":""Bob"",""Owes"":{},""OwedBy"":{},""Balance"":0}]"
         Assert.Equal(expected, actual)
     End Sub
 End Class
