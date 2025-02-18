f231ff6afcba2e6154ae9f497426a0807c8a3c21
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-02-17 21:53:48 -0800

    Add `bottle-song`

---
+++ exercises/practice/bottle-song/BottleSong.vb
@@ -0,0 +1,7 @@
+Imports System
+
+Public Module BottleSong
+    Public Function Recite(ByVal startBottles As Integer, ByVal takeDown As Integer) As String
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+End Module
