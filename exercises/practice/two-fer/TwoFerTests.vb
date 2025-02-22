49971c15b599ea8dffecd5c0c62dc22883bcdec0
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-02-22 11:32:33 -0800

    Resolve ambiguous .Equal overloads

--- exercises/practice/two-fer/TwoFerTests.vb
+++ exercises/practice/two-fer/TwoFerTests.vb
@@ -2,16 +2,16 @@ Imports Xunit
 Public Class TwoFerTests
     <Fact>
     Public Sub NoNameGiven()
-        Assert.Equal("One for you, one for me.", TwoFer.Speak())
+        Assert.Equal("One for you, one for me.", TwoFer.Speak(), false)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub ANameGiven()
-        Assert.Equal("One for Alice, one for me.", TwoFer.Speak("Alice"))
+        Assert.Equal("One for Alice, one for me.", TwoFer.Speak("Alice"), false)
     End Sub
   
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub AnotherNameGiven()
-        Assert.Equal("One for Bob, one for me.", TwoFer.Speak("Bob"))
+        Assert.Equal("One for Bob, one for me.", TwoFer.Speak("Bob"), false)
     End Sub
 End Class
