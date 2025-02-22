61001c30f0b369b3e67cee319b51dc86a7a7745b
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-02-22 11:25:07 -0800

    Flip expected and actual order for `bob` tests

--- exercises/practice/bob/BobTests.vb
+++ exercises/practice/bob/BobTests.vb
@@ -1,90 +1,90 @@
-Imports XUnit
-
+Imports Xunit
 
 Public Class BobTest
     Private teenager = New Bob()
+
     <Fact>
     Public Sub StatingSomething()
-        Assert.Equal(teenager.Hey("Tom-ay-to, tom-aaaah-to."), "Whatever.")
+        Assert.Equal("Whatever.", teenager.Hey("Tom-ay-to, tom-aaaah-to."))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub Shouting()
-        Assert.Equal(teenager.Hey("WATCH OUT!"), "Whoa, chill out!")
+        Assert.Equal("Whoa, chill out!", teenager.Hey("WATCH OUT!"))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub AskingAQuestion()
-        Assert.Equal(teenager.Hey("Does this cryogenic chamber make me look fat?"), "Sure.")
+        Assert.Equal("Sure.", teenager.Hey("Does this cryogenic chamber make me look fat?"))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub AskingANumericQuestion()
-        Assert.Equal(teenager.Hey("You are, what, like 15?"), "Sure.")
+        Assert.Equal("Sure.", teenager.Hey("You are, what, like 15?"))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub TalkingForcefully()
-        Assert.Equal(teenager.Hey("Let's go make out behind the gym!"), "Whatever.")
+        Assert.Equal("Whatever.", teenager.Hey("Let's go make out behind the gym!"))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub UsingAcronymsInRegularSearch()
-        Assert.Equal(teenager.Hey("It's OK if you don't want to go to the DMV."), "Whatever.")
+        Assert.Equal("Whatever.", teenager.Hey("It's OK if you don't want to go to the DMV."))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub ForcefulQuestions()
-        Assert.Equal(teenager.Hey("WHAT THE HELL WERE YOU THINKING?"), "Calm down, I know what I'm doing!")
+        Assert.Equal("Calm down, I know what I'm doing!", teenager.Hey("WHAT THE HELL WERE YOU THINKING?"))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub ShoutingNumbers()
-        Assert.Equal(teenager.Hey("1, 2, 3 GO!"), "Whoa, chill out!")
+        Assert.Equal("Whoa, chill out!", teenager.Hey("1, 2, 3 GO!"))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub OnlyNumbers()
-        Assert.Equal(teenager.Hey("1, 2, 3"), "Whatever.")
+        Assert.Equal("Whatever.", teenager.Hey("1, 2, 3"))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub QuestionWithOnlyNumbers()
-        Assert.Equal(teenager.Hey("4?"), "Sure.")
+        Assert.Equal("Sure.", teenager.Hey("4?"))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub ShoutingWithSpecialCharacters()
-        Assert.Equal(teenager.Hey("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!"), "Whoa, chill out!")
+        Assert.Equal("Whoa, chill out!", teenager.Hey("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!"))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub ShoutingWithNoExclamationMark()
-        Assert.Equal(teenager.Hey("I HATE YOU"), "Whoa, chill out!")
+        Assert.Equal("Whoa, chill out!", teenager.Hey("I HATE YOU"))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub StatementContainingQuestionMark()
-        Assert.Equal(teenager.Hey("Ending with ? means a question."), "Whatever.")
+        Assert.Equal("Whatever.", teenager.Hey("Ending with ? means a question."))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub PrattlingOn()
-        Assert.Equal(teenager.Hey("Wait! Hang on. Are you going to be OK?"), "Sure.")
+        Assert.Equal("Sure.", teenager.Hey("Wait! Hang on. Are you going to be OK?"))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub Silence()
-        Assert.Equal(teenager.Hey(""), "Fine. Be that way!")
+        Assert.Equal("Fine. Be that way!", teenager.Hey(""))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub ProlongedSilence()
-        Assert.Equal(teenager.Hey("    "), "Fine. Be that way!")
+        Assert.Equal("Fine. Be that way!", teenager.Hey("    "))
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub MultipleLineQuestion()
-        Assert.Equal(teenager.Hey("Does this cryogenic chamber make me look fat?" & vbLf & "no"), "Whatever.")
+        Assert.Equal("Whatever.", teenager.Hey("Does this cryogenic chamber make me look fat?" & vbLf & "no"))
     End Sub
 End Class
