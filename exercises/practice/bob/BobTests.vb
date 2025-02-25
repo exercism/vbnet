de51f49bdbfe5b5a4ae8e98579d51ad6082ab370
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-02-24 21:07:50 -0800

    Specify result type as string

--- exercises/practice/bob/BobTests.vb
+++ exercises/practice/bob/BobTests.vb
@@ -5,86 +5,120 @@ Public Class BobTest
 
     <Fact>
     Public Sub StatingSomething()
-        Assert.Equal("Whatever.",  teenager.Hey("Tom-ay-to, tom-aaaah-to."), false)
+        Dim expected = "Whatever."
+        Dim result as String = teenager.Hey("Tom-ay-to, tom-aaaah-to.")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub Shouting()
+        Dim expected = "Whoa, chill out!"
+        Dim result as String = teenager.Hey("WATCH OUT!")
         Assert.Equal("Whoa, chill out!", teenager.Hey("WATCH OUT!"), false)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub AskingAQuestion()
-        Assert.Equal("Sure.", teenager.Hey("Does this cryogenic chamber make me look fat?"), false)
+        Dim expected = "Sure."
+        Dim result as String = teenager.Hey("Does this cryogenic chamber make me look fat?")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub AskingANumericQuestion()
-        Assert.Equal("Sure.", teenager.Hey("You are, what, like 15?"), false)
+        Dim expected = "Sure."
+        Dim result as String = teenager.Hey("You are, what, like 15?")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub TalkingForcefully()
-        Assert.Equal("Whatever.", teenager.Hey("Let's go make out behind the gym!"), false)
+        Dim expected = "Whatever."
+        Dim result as String = teenager.Hey("Let's go make out behind the gym!")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub UsingAcronymsInRegularSearch()
-        Assert.Equal("Whatever.", teenager.Hey("It's OK if you don't want to go to the DMV."), false)
+        Dim expected = "Whatever."
+        Dim result as String = teenager.Hey("It's OK if you don't want to go to the DMV.")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub ForcefulQuestions()
-        Assert.Equal("Calm down, I know what I'm doing!", teenager.Hey("WHAT THE HELL WERE YOU THINKING?"), false)
+        Dim expected = "Calm down, I know what I'm doing!"
+        Dim result as String = teenager.Hey("WHAT THE HELL WERE YOU THINKING?")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub ShoutingNumbers()
-        Assert.Equal("Whoa, chill out!", teenager.Hey("1, 2, 3 GO!"), false)
+        Dim expected = "Whoa, chill out!"
+        Dim result as String = teenager.Hey("1, 2, 3 GO!")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub OnlyNumbers()
-        Assert.Equal("Whatever.", teenager.Hey("1, 2, 3"), false)
+        Dim expected = "Whatever."
+        Dim result as String = teenager.Hey("1, 2, 3")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub QuestionWithOnlyNumbers()
-        Assert.Equal("Sure.", teenager.Hey("4?"), false)
+        Dim expected = "Sure."
+        Dim result as String = teenager.Hey("4?")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub ShoutingWithSpecialCharacters()
-        Assert.Equal("Whoa, chill out!", teenager.Hey("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!"), false)
+        Dim expected = "Whoa, chill out!"
+        Dim result as String = teenager.Hey("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub ShoutingWithNoExclamationMark()
-        Assert.Equal("Whoa, chill out!", teenager.Hey("I HATE YOU"), false)
+        Dim expected = "Whoa, chill out!"
+        Dim result as String = teenager.Hey("I HATE YOU")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub StatementContainingQuestionMark()
-        Assert.Equal("Whatever.", teenager.Hey("Ending with ? means a question."), false)
+        Dim expected = "Whatever."
+        Dim result as String = teenager.Hey("Ending with ? means a question.")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub PrattlingOn()
-        Assert.Equal("Sure.", teenager.Hey("Wait! Hang on. Are you going to be OK?"), false)
+        Dim expected = "Sure."
+        Dim result as String = teenager.Hey("Wait! Hang on. Are you going to be OK?")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub Silence()
-        Assert.Equal("Fine. Be that way!", teenager.Hey(""), false)
+        Dim expected = "Fine. Be that way!"
+        Dim result as String = teenager.Hey("")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub ProlongedSilence()
-        Assert.Equal("Fine. Be that way!", teenager.Hey("    "), false)
+        Dim expected = "Fine. Be that way!"
+        Dim result as String = teenager.Hey("    ")
+        Assert.Equal(expected, result)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
     Public Sub MultipleLineQuestion()
-        Assert.Equal("Whatever.", teenager.Hey("Does this cryogenic chamber make me look fat?" & vbLf & "no"), false)
+        Dim expected = "Whatever."
+        Dim result as String = teenager.Hey("Does this cryogenic chamber make me look fat?" & vbLf & "no")
+        Assert.Equal(expected, result)
     End Sub
 End Class
