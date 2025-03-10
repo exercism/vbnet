029ce481963874d4c9adb50b1c71fac336d92711
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-03-09 17:03:10 -0700

    Sync `resistor-color-trio` (#362)

--- exercises/practice/resistor-color-trio/.meta/Example.vb
+++ exercises/practice/resistor-color-trio/.meta/Example.vb
@@ -1,22 +1,38 @@
 Imports System
 
 Public Module ResistorColorTrio
-    Private Const KiloOhms As Integer = 1_000
     Private ReadOnly AllColors As String() = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}
+
     Public Function Label(ByVal colors As String()) As String
-        Dim value = GetValue(colors)
+        Dim value As Long = GetValue(colors)
         Dim unit = GetUnit(value)
-        If unit = "kiloohms" Then value \= 1_000
+        If unit = "kiloohms" Then
+            value \= 1_000
+        ElseIf unit = "megaohms" Then
+            value \= 1_000_000
+        ElseIf unit = "gigaohms" Then
+            value \= 1_000_000_000
+        End If
         Return String.Format("{0} {1}", value, unit)
-    End Function    
-    Private Function GetValue(ByVal colors As String()) As Integer
-        Return (ResistorValue(colors(0)) * 10 + ResistorValue(colors(1))) * CInt(Math.Pow(10, ResistorValue(colors(2))))
     End Function
-    Private Function GetUnit(ByVal value As Integer) As String
-        Return If(value >= KiloOhms, "kilo", "") & "ohms"
+
+    Private Function GetValue(ByVal colors As String()) As Long
+        Return (ResistorValue(colors(0)) * 10 + ResistorValue(colors(1))) * CLng(Math.Pow(10, ResistorValue(colors(2))))
     End Function
+
+    Private Function GetUnit(ByVal value As Long) As String
+        If value >= 1_000_000_000 Then
+            Return "gigaohms"
+        ElseIf value >= 1_000_000 Then
+            Return "megaohms"
+        ElseIf value >= 1_000 Then
+            Return "kiloohms"
+        Else
+            Return "ohms"
+        End If
+    End Function
+
     Private Function ResistorValue(ByVal color As String) As Integer
         Return Array.IndexOf(AllColors, color)
     End Function
-
 End Module
--- exercises/practice/resistor-color-trio/ResistorColorTrioTests.vb
+++ exercises/practice/resistor-color-trio/ResistorColorTrioTests.vb
@@ -25,4 +25,29 @@ Public Class ResistorColorTrioTests
     Public Sub Yellow_and_violet_and_yellow()
         Assert.Equal("470 kiloohms", Label({"yellow", "violet", "yellow"}))
     End Sub
+
+    <Fact(Skip:="Remove this Skip property to run this test")>
+    Public Sub Blue_and_violet_and_blue()
+        Assert.Equal("67 megaohms", Label({"blue", "violet", "blue"}))
+    End Sub
+
+    <Fact(Skip:="Remove this Skip property to run this test")>
+    Public Sub Minimum_possible_value()
+        Assert.Equal("0 ohms", Label({"black", "black", "black"}))
+    End Sub
+
+    <Fact(Skip:="Remove this Skip property to run this test")>
+    Public Sub Maximum_possible_value()
+        Assert.Equal("99 gigaohms", Label({"white", "white", "white"}))
+    End Sub
+
+    <Fact(Skip:="Remove this Skip property to run this test")>
+    Public Sub First_two_colors_make_an_invalid_octal_number()
+        Assert.Equal("8 ohms", Label({"black", "grey", "black"}))
+    End Sub
+
+    <Fact(Skip:="Remove this Skip property to run this test")>
+    Public Sub Ignore_extra_colors()
+        Assert.Equal("650 kiloohms", Label({"blue", "green", "yellow", "orange"}))
+    End Sub
 End Class
