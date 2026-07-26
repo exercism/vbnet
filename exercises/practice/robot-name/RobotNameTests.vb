9bc0efea790a8c64df1b8efd54f5d2b13d2c0fb3
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-07-26 09:13:26 -0700

    Simplified construction

--- exercises/practice/robot-name/RobotNameTests.vb
+++ exercises/practice/robot-name/RobotNameTests.vb
@@ -4,7 +4,7 @@ Imports System.Linq
 Imports Xunit
 
 Public Class RobotNameTests
-    Private ReadOnly robot As Robot = New Robot()
+    Private ReadOnly robot As Robot = New()
 
     <Fact>
     Public Sub Robot_has_a_name()
