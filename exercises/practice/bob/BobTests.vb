5ef73232bf07fa4a2eb40c3f3eb1857686d68469
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-07-26 09:11:03 -0700

    Remove unnecessary assignment

--- exercises/practice/bob/BobTests.vb
+++ exercises/practice/bob/BobTests.vb
@@ -14,7 +14,7 @@ Public Class BobTest
     Public Sub Shouting()
         Dim expected = "Whoa, chill out!"
         Dim result as String = teenager.Hey("WATCH OUT!")
-        Assert.Equal("Whoa, chill out!", teenager.Hey("WATCH OUT!"), false)
+        Assert.Equal(expected, actual)
     End Sub
 
     <Fact(Skip:="Remove this Skip property to run this test")>
