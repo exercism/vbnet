e0fa23db2a85eeaa7f98e79d34b8c79895727b16
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-08-25 10:56:06 -0700

    add `line-up`

---
+++ exercises/practice/line-up/LineUp.vb
@@ -0,0 +1,5 @@
+Public Module LineUp
+    Public Function Format(ByVal name As String, ByVal number As Integer) As String
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+End Module
