a378687e73fedd51a1d81081ff5edc616cf9778f
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-08-25 08:07:42 -0700

    Add `state-of-tic-tac-toe`

---
+++ exercises/practice/state-of-tic-tac-toe/StateOfTicTacToe.vb
@@ -0,0 +1,11 @@
+Public Enum State
+    Win
+    Draw
+    Ongoing
+End Enum
+
+Public Module StateOfTicTacToe
+    Public Function Gamestate(ByVal board As String()) As State
+        Throw New NotImplementedException("You need to implement this function.")
+    End Function
+End Module
