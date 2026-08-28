3935b5dfc060cb627ad047025405d216bb98def3
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-08-28 16:42:20 -0700

    Add `custom-set` (#575)

---
+++ exercises/practice/custom-set/CustomSet.vb
@@ -0,0 +1,45 @@
+Public Class CustomSet
+    Public Sub New(ParamArray values As Integer())
+        Throw New NotImplementedException("You need to implement this method.")
+    End Sub
+
+    Public Function Add(ByVal value As Integer) As CustomSet
+        Throw New NotImplementedException("You need to implement this method.")
+    End Function
+
+    Public Function Empty() As Boolean
+        Throw New NotImplementedException("You need to implement this method.")
+    End Function
+
+    Public Function Contains(ByVal value As Integer) As Boolean
+        Throw New NotImplementedException("You need to implement this method.")
+    End Function
+
+    Public Function Subset(ByVal other As CustomSet) As Boolean
+        Throw New NotImplementedException("You need to implement this method.")
+    End Function
+
+    Public Function Disjoint(ByVal other As CustomSet) As Boolean
+        Throw New NotImplementedException("You need to implement this method.")
+    End Function
+
+    Public Function Intersection(ByVal other As CustomSet) As CustomSet
+        Throw New NotImplementedException("You need to implement this method.")
+    End Function
+
+    Public Function Difference(ByVal other As CustomSet) As CustomSet
+        Throw New NotImplementedException("You need to implement this method.")
+    End Function
+
+    Public Function Union(ByVal other As CustomSet) As CustomSet
+        Throw New NotImplementedException("You need to implement this method.")
+    End Function
+
+    Public Overrides Function Equals(ByVal obj As Object) As Boolean
+        Throw New NotImplementedException("You need to implement this method.")
+    End Function
+
+    Public Overrides Function GetHashCode() As Integer
+        Throw New NotImplementedException("You need to implement this method.")
+    End Function
+End Class
