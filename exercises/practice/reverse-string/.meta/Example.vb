7e50a1ab4ef0cad69d48d7edd8f9f74e32fed94e
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-08-28 08:03:04 -0700

    Generator and updated example for `reverse-string` (#571)

--- exercises/practice/reverse-string/.meta/Example.vb
+++ exercises/practice/reverse-string/.meta/Example.vb
@@ -1,5 +1,13 @@
 Public Module ReverseString
     Public Function Reverse(ByVal inputString As String) As String
-        Return New String(inputString.ToCharArray().Reverse().ToArray())
+        Dim elements = New List(Of String)()
+        Dim enumerator = Globalization.StringInfo.GetTextElementEnumerator(inputString)
+
+        While enumerator.MoveNext()
+            elements.Add(enumerator.GetTextElement())
+        End While
+
+        elements.Reverse()
+        Return String.Concat(elements)
     End Function
 End Module
