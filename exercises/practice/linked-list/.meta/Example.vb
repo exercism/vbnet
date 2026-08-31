2dd3def68049b21ce4a7d66f9407ff18eb43925e
KenX049 <89333223+ManasDasri@users.noreply.github.com>
2026-08-31 22:38:34 +0530

    Sync linked-list with problem specifications (#562)
    * Sync linked-list with problem specifications
    
    * Restore linked-list exercise stub
    
    * Update linked-list exercise stub API

--- exercises/practice/linked-list/.meta/Example.vb
+++ exercises/practice/linked-list/.meta/Example.vb
@@ -1,51 +1,103 @@
 Public Class Deque(Of T)
     Private head As Element
+    Private tail As Element
+    Private itemCount As Integer
+
+    Public ReadOnly Property Count As Integer
+        Get
+            Return itemCount
+        End Get
+    End Property
 
     Public Sub Push(ByVal value As T)
-        If head Is Nothing Then
-            head = New Element(value)
+        Dim element = New Element(value)
+
+        If tail Is Nothing Then
+            head = element
+            tail = element
         Else
-            Dim last = head.Next
-            Dim e = New Element(value, last, head)
-            last.Prev = e
-            head.Next = e
+            element.Prev = tail
+            tail.[Next] = element
+            tail = element
         End If
+
+        itemCount += 1
     End Sub
 
     Public Function Pop() As T
-        head = head.Next
-        Return Shift()
+        Dim value = tail.Value
+
+        If tail Is head Then
+            head = Nothing
+            tail = Nothing
+        Else
+            tail = tail.Prev
+            tail.[Next] = Nothing
+        End If
+
+        itemCount -= 1
+        Return value
     End Function
 
     Public Sub Unshift(ByVal value As T)
-        Push(value)
-        head = head.Next
+        Dim element = New Element(value)
+
+        If head Is Nothing Then
+            head = element
+            tail = element
+        Else
+            element.[Next] = head
+            head.Prev = element
+            head = element
+        End If
+
+        itemCount += 1
     End Sub
 
     Public Function Shift() As T
         Dim value = head.Value
-        Dim last = head.Next
 
-        If last Is head Then
+        If head Is tail Then
             head = Nothing
+            tail = Nothing
         Else
-            last.Prev = head.Prev
-            head.Prev.Next = last
-            head = head.Prev
+            head = head.[Next]
+            head.Prev = Nothing
         End If
 
+        itemCount -= 1
         Return value
     End Function
 
+    Public Sub Delete(ByVal value As T)
+        Dim current = head
+
+        While current IsNot Nothing
+            If EqualityComparer(Of T).Default.Equals(current.Value, value) Then
+                If current Is head Then
+                    Shift()
+                ElseIf current Is tail Then
+                    Pop()
+                Else
+                    current.Prev.[Next] = current.[Next]
+                    current.[Next].Prev = current.Prev
+                    itemCount -= 1
+                End If
+
+                Return
+            End If
+
+            current = current.[Next]
+        End While
+    End Sub
+
     Private Class Element
         Public ReadOnly Value As T
-        Public [Next] As Element
-        Public Prev As Element
+        Public Property [Next] As Element
+        Public Property Prev As Element
 
-        Public Sub New(ByVal value As T, ByVal Optional [next] As Element = Nothing, ByVal Optional prev As Element = Nothing)
+        Public Sub New(ByVal value As T)
             Me.Value = value
-            Me.Next = If([next], Me)
-            Me.Prev = If(prev, Me)
         End Sub
     End Class
 End Class
--- exercises/practice/linked-list/LinkedList.vb
+++ exercises/practice/linked-list/LinkedList.vb
@@ -1,17 +1,26 @@
 Public Class Deque(Of T)
-    Public Sub PushMethod(ByVal value As T)
+    Public Sub Push(ByVal value As T)
         Throw New NotImplementedException("You need to implement this function.")
     End Sub
-
     Public Function Pop() As T
         Throw New NotImplementedException("You need to implement this function.")
     End Function
 
-    Public Sub UnshiftMethod(ByVal value As T)
+    Public Sub Unshift(ByVal value As T)
         Throw New NotImplementedException("You need to implement this function.")
     End Sub
 
     Public Function Shift() As T
         Throw New NotImplementedException("You need to implement this function.")
     End Function
+
+    Public ReadOnly Property Count As Integer
+        Get
+            Throw New NotImplementedException("You need to implement this function.")
+        End Get
+    End Property
+
+    Public Sub Delete(ByVal value As T)
+        Throw New NotImplementedException("You need to implement this function.")
+    End Sub
 End Class
